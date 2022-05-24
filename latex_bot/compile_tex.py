78db6875ab62ca61761e03bc1cb953d4d317d74c
Indrajit <indrajitghosh912@gmail.com>
2022-05-24 15:45:32 +0530

    bug fixed

---
+++ latex_bot/compile_tex.py
@@ -0,0 +1,93 @@
+# A script to compile a tex file
+#
+# Author: Indrajit Ghosh
+#
+# Date: May 24, 2022
+#
+
+from pathlib import Path
+import subprocess, sys
+
+
+HOME = Path.home()
+CWD = Path.cwd()
+
+def list_files(path):
+    """
+    Returns the lists containg AbsolutePaths of files
+    at the given path
+    """
+    given_path = Path.absolute(path)
+    files = []
+
+    for file_or_dir in given_path.glob('*'):
+        if file_or_dir.is_file():
+            files.append(file_or_dir)
+
+    # p = Path.absolute(files[0]) # Getting the absolute path
+
+    return files
+
+
+def compile_tex(texfilepath:Path):
+    """
+    This function takes a texfile's absolute Path and compiles
+    it in the following manner:
+
+        pdflatex <main.tex>
+        bibtex <main.aux>
+        pdflatex <main.tex>
+        pdflatex <main.tex>
+    
+    """
+
+    texfilepath = Path(texfilepath)
+
+    parent_dir = texfilepath.parent
+    texfilename = texfilepath.name
+
+    subprocess.run(["pdflatex", texfilename]) # compiling `main.tex`
+
+    auxfilename = texfilename[:-4] + ".aux"
+
+    subprocess.run(["bibtex", auxfilename]) # compiling `main.aux`
+
+    subprocess.run(["pdflatex", texfilename]) # compiling `main.tex`
+    subprocess.run(["pdflatex", texfilename]) # compiling `main.tex`
+
+    print("\n\n:::NOTE::: Successfully complied!\n\n")
+
+
+    pdfname = texfilename[:-4] + ".pdf"
+    subprocess.run(['xdg-open', pdfname]) # opening `main.pdf`
+
+
+def main():
+
+    if len(sys.argv) < 2:
+
+        main_tex_file = CWD / "main.tex"
+
+        if main_tex_file.exists():
+            compile_tex(main_tex_file)
+
+        else:
+            # Usages
+            print(
+                """
+                USAGES: compile_tex.py
+
+                    compiletex <main.tex>
+
+                """
+            )
+            sys.exit()
+
+    else:
+        texfile = CWD / sys.argv[1]
+
+        compile_tex(texfile)
+
+
+if __name__ == '__main__':
+    main()
--- latex_bot/tex_bot.py
+++ latex_bot/tex_bot.py
@@ -10,6 +10,8 @@
 
 from functions import *
 
+COMPILE_TEX_LIVES_HERE = HOME / "Documents/latex_files/scripts_for_tex/latex_bot/compile_tex.py"
+
 
 def main():
 
@@ -33,9 +35,18 @@ def main():
 
     output_dir_path = setup_output_directory(res)
 
+    # Creating tex, bib, ist etc files
     create_math_constants_tex(output_dir_path)
     create_structure_tex(tex_template=res, output_dir=output_dir_path)
     create_main_tex(tex_template=res, info=informations, output_dir=output_dir_path)
+
+    # Compiling the `main.tex` file
+    os.chdir(output_dir_path)
+    subprocess.run(["python3", str(COMPILE_TEX_LIVES_HERE)])
+
+    os.system('clear')
+
+    print(f"\n\n\t\t::: YOUR TeX WORKING DIRECTORY :::\n\n   {output_dir_path}\n\n")
 
 
 if __name__ == '__main__':
