5f1293b0b7de3941ccf05decfc4dec48d12b4644
Indrajit <indrajitghosh912@gmail.com>
2023-06-17 18:05:53 +0530

    TexFile created

---
+++ latex_bot/latex.py
@@ -0,0 +1,151 @@
+# A module for LaTeX
+#
+# Author: Indrajit Ghosh
+#
+# Date: Jun 17, 2023
+#
+
+
+class TexFile:
+    """
+    TeX templates used to create `plainarticle`, `article`, `amsarticle` etc
+
+    Parameters
+    ----------
+    tex_compiler : :class:`str`
+        The TeX compiler to be used, e.g. ``latex``, ``pdflatex`` or ``lualatex``
+    output_format : :class:`str`
+        The output format resulting from compilation, e.g. ``.dvi`` or ``.pdf``
+    documentclass : :class:`str`
+        The command defining the documentclass, e.g. ``\\documentclass[preview]{standalone}``
+    preamble : :class:`str`
+        The document's preamble, i.e. the part between ``\\documentclass`` and ``\\begin{document}``
+    body_text : :class:`str`
+        Text in between ``\\begin{document}`` and ``\\end{document}``
+    pre_doc_commands : :class:`str`
+        Text to be inserted at the very begining of the file, e.g. ``\\newcommand{\Author}{Indrajit Ghosh}``
+    post_doc_commands : :class:`str`
+        Text (definitions, commands) to be inserted at right after ``\\begin{document}``, e.g. ``\\boldmath``
+    """
+
+    default_documentclass = r"\documentclass[12pt, twoside]{article}"
+    default_preamble = r"""
+\usepackage[english]{babel}
+\usepackage[top=1 in,bottom=1in, left=1 in, right=1 in]{geometry}
+"""
+    default_body_text = "YourTextHere"
+    default_tex_compiler = "pdflatex"
+    default_output_format = ".pdf"
+    default_pre_doc_commands = ""
+    default_post_doc_commands = ""
+
+
+    def __init__(
+            self,
+            tex_compiler:str=None,
+            output_format:str=None,
+            documentclass:str=None,
+            preamble:str=None,
+            body_text:str=None,
+            pre_doc_commands:str=None,
+            post_doc_commands:str=None,
+            **kwargs,
+    ):
+        self._tex_compiler = (
+            tex_compiler
+            if tex_compiler is not None
+            else TexFile.default_tex_compiler
+        )
+
+        self._output_format = (
+            output_format
+            if output_format is not None
+            else TexFile.default_output_format
+        )
+
+        self._documentclass = (
+            documentclass
+            if documentclass is not None
+            else TexFile.default_documentclass
+        )
+
+        self._preamble = (
+            preamble
+            if preamble is not None
+            else TexFile.default_preamble
+        )
+
+        self._body_text = (
+            body_text
+            if body_text is not None
+            else TexFile.default_body_text
+        )
+
+        self._pre_doc_commands = (
+            pre_doc_commands
+            if pre_doc_commands is not None
+            else TexFile.default_pre_doc_commands
+        )
+
+        self._post_doc_commands = (
+            post_doc_commands
+            if post_doc_commands is not None
+            else TexFile.default_post_doc_commands
+        )
+
+
+    @property
+    def tex_complier(self):
+        return self._tex_compiler
+    
+    @tex_complier.setter
+    def tex_compiler(self, compiler:str):
+        self._tex_compiler = compiler
+
+    @property
+    def output_format(self):
+        return self._output_format
+
+    @output_format.setter
+    def output_format(self, new_format:str):
+        self._output_format = new_format
+
+    @property
+    def documentclass(self):
+        return self._documentclass
+
+    @documentclass.setter
+    def documentclass(self, newclass:str):
+        self._documentclass = newclass
+
+    @property
+    def preamble(self):
+        return self._preamble
+    
+    @preamble.setter
+    def preamble(self, newpreamble:str):
+        self._preamble = newpreamble
+
+    @property
+    def body_text(self):
+        return self._body_text
+    
+    @body_text.setter
+    def body_text(self, newtext:str):
+        self._body_text = newtext
+
+    @property
+    def pre_doc_commands(self):
+        return self._pre_doc_commands
+
+    @pre_doc_commands.setter
+    def pre_doc_commands(self, newcmds:str):
+        self._pre_doc_commands = newcmds
+
+    @property
+    def post_doc_commands(self):
+        return self._post_doc_commands
+    
+    @post_doc_commands.setter
+    def post_doc_commands(self, newcmds:str):
+        self._post_doc_commands = newcmds
