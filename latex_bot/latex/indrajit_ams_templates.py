bcbcf8fce2e730e4ab973984662111f35d323e9a
Indrajit <indrajitghosh912@gmail.com>
2023-07-21 14:57:08 +0530

    created

---
+++ latex_bot/latex/indrajit_ams_templates.py
@@ -0,0 +1,10 @@
+# A library containing all packages, theorem styles
+# and custom commands that I usually use in `amsart`
+#
+# Author: Indrajit Ghosh
+# Date: Jul 21, 2023
+#
+
+
+class IndraAmsPackages:
+    pass
