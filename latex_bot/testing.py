676bd96fc504ddabaace43cf3bed440fbe26c545
Indrajit <indrajitghosh912@gmail.com>
2023-07-30 16:57:44 +0530

    updated

--- latex_bot/testing.py
+++ latex_bot/testing.py
@@ -6,12 +6,19 @@ from latex import *
 
 def main():
 
-    plainart = PlainArticle()
-    plainart._tex_template = TexFontTemplates.libris_adf_fourier
+    msg = r"""
+Hi, I am Indrajit...
+
+Here is some Math equation:
+\[
+    \int f\ d\mu = 0
+\]
+"""
+    plainart = PlainArticle(body_text=msg)
+    plainart._tex_template = TexFontTemplates.slitex
 
     plainart.show_output()
     
-
 
 if __name__ == '__main__':
     main()
